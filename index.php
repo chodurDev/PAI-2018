33cfdfdfe3c514e4de550f435939139d47c088ce
chodurDev <35374811+chodurDev@users.noreply.github.com>
2019-01-02 20:31:44 +0100

    logowanie,łączenie z bazą danych oraz poprawa drzewa projektu

--- index.php
+++ index.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html>
+<?php
 
-<head>
-    <title>44 Detailing-CRM</title>
-    <link rel="stylesheet" type="text/css" href="public/css/style.css" />
-
-</head>
-
-<body>
-<div>
-    <form action="login.php" method="post">
-        <input class="button-start" type="submit" value="">
-    </form>
-</div>
+require_once ('Routing.php');
+$routing = new Routing();
+$routing->run();
 
 
-</body>
-</html>
+
+
